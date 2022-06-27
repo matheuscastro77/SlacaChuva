010b69ba9a762896a1b220ee9ad2e59c725e7531
matheuscastro77 <you@example.com>
2022-06-27 14:43:02 -0300

    FEAT: commit inicial com a criação da estrutura do projeto

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const RoutesApp = () => {
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
+    )
+    
+}
+
+export default RoutesApp
